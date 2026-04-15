a4701b46a1c5ede5acd64c96c8c64e3a899c1059
I1HN <gabrielgalbiati010@gmail.com>
2026-04-15 15:32:44 -0300

    Comentando atividade 4

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let senhaUm = prompt("Insira a senha para ter acesso a sua conta bancária: ");
 let senhaDois = prompt("Insira a senah novamente para passar pelo processo de segurança");
 
+
+// O IF e ELES serve para ter duas alternativas, se é isso não é aquilo 
 if (senhaDois == senhaUm) {
     alert("acesso permitido")
 } else {
