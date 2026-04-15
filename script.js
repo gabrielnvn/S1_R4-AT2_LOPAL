2ccbc3609d927b11a7e370035e076eb945fb2f6b
I1HN <gabrielgalbiati010@gmail.com>
2026-04-15 12:18:06 -0300

    averiguando a senha do usuário

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let senhaUm = prompt("Insira a senha para ter acesso a sua conta bancária: ")
+let senhaUm = prompt("Insira a senha para ter acesso a sua conta bancária: ");
+let senhaDois = prompt("Insira a senah novamente para passar pelo processo de segurança");
 
+if (senhaDois == senhaUm) {
+    alert("acesso permitido")
+} else {
+    alert("Senha incorreta, tente novamente")
+}
     
 
 
