a9191d5c97ab8a9091648b3b593b07b4a85551ea
I1HN <gabrielgalbiati010@gmail.com>
2026-04-15 15:22:23 -0300

    Comentando a atividade

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// Usei o parseInt para converter o numero que o usuário comentar para numero inteiro
 let numberUm = parseInt(prompt("Escreva um numero par:"));
 
+// usei o isNaN para caso o usuário não escreva um numero
+// IF e ELSE serve para dar alternativas diferentes, se for isso, não vai ser aquilo
 if (isNaN(numberUm)) {
     alert("Você não digitou um numero")
 } else {
@@ -10,10 +13,3 @@ if (isNaN(numberUm)) {
 }
     
 }
- 
-
-
-
-
-
-   
