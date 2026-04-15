edcdcff42b5e06fa08e195571ad20e0bfc7b3c4d
I1HN <gabrielgalbiati010@gmail.com>
2026-04-15 08:47:22 -0300

    Identificando se o numero é par ou não

---
+++ script.js
@@ -0,0 +1,19 @@
+let numberUm = parseInt(prompt("Escreva um numero par:"));
+
+if (isNaN(numberUm)) {
+    alert("Você não digitou um numero")
+} else {
+    if (numberUm %2 == 0) {
+    alert("Seu numero é par!")
+} else {
+    alert(" Seu numero não é par!")
+}
+    
+}
+ 
+
+
+
+
+
+   
