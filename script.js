416d0628af5a737a40f18ff32c45a17eb68720af
I1HN <gabrielgalbiati010@gmail.com>
2026-04-15 10:03:05 -0300

    Aplicando um desconto ou cashback de acordo com o preço da compra

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-let numberUm = parseInt(prompt("Escreva um numero par:"));
+let valorUm = Number(prompt("Escreva um quanto deu a sua compra:"));
 
-if (isNaN(numberUm)) {
-    alert("Você não digitou um numero")
+if (isNaN(valorUm)) {
+    alert("Por favor insira um numero!!!")
 } else {
-    if (numberUm %2 == 0) {
-    alert("Seu numero é par!")
-} else {
-    alert(" Seu numero não é par!")
+    if (valorUm >= 150) {
+        alert(`como seu valor foi acima de R$ 150,00 você recebeu um desconto de 2%, sua compra ficou em um total de ${(valorUm - valorUm*2/100).toFixed(2)} reais`)
+    } else {
+        alert(`como sua compra ficou abaixo de R$ 150,00 você receberá um cashback de ${(valorUm*2/100).toFixed(2)} reais, use-o na sua proxima compra `)
+    }
 }
-    
-}
- 
-
-
 
 
 
